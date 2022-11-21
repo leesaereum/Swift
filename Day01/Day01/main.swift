324e0db15388753467d2764b9158231d349c0216
Rumy <52562826+leesaereum@users.noreply.github.com>
2022-11-21 11:29:33 +0900

    Add files via upload

---
+++ Day01/Day01/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+/*
+ -------------------------
+ 상수나 변수의 작명 규칙
+ -------------------------
+ - Camel Case : function, method, variable, constant...
+ ex) studentName  ,studentAge ....
+ 
+ - Pascal Case : class,struct, enum, extension
+ ex) StudentName, StudnetAge...
+ 
+ - Snake Case: underbar(_)로 연결하여 사용
+ ex.)student_name, student_age
+ 
+ * 대소문자 구분
+ *첫 글자를 숫자나 특수문자로 시작하면 안되나 _(underbar)는 사용가능
+ (* underbar의 용도는 따로 존재함)
+ */
+
+// 상수를 이용한 출력
+let message = "안녕하세요"
+//message = "Good morning"
+print(message)
+print(message)
